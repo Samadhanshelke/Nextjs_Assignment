c40d1513f5e16ac7d79ba73f00e264c1b1478d2b
Samadhanshelke <96407937+Samadhanshelke@users.noreply.github.com>
2024-05-11 15:05:53 +0530

    snap added

--- app/components/HeroN.js
+++ app/components/HeroN.js
@@ -8,25 +8,24 @@ import msg2 from '../Images/msg2.svg'
 import data from "../utils/data";
 function Hero() {
   return (
-    <div className='sm:h-[70vh]  w-[400px]  sm:w-full overflow-x-scroll overflow-y-hidden  sm:overflow-x-hidden flex flex-row sm:flex-col  sm:gap-y-8  sm:overflow-y-scroll scroll-container' >
+    <div className='w-full overflow-x-scroll flex sm:flex-col gap-y-8 gap-x-8 sm:h-[70vh] sm:overflow-x-hidden scroll-container' >
       {data.map((item) => {
         return (
-          <div key={item.id} className="flex   min-w-[400px] p-6 w-full  justify-center gap-x-8  flex-col gap-y-4 items-center sm:flex-row m-auto  scroll-item">
+          <div key={item.id} className="min-w-full flex flex-col sm:flex-row gap-y-4  items-center justify-center gap-x-8 scroll-item">
           {
             item.id === 1 
             ? <Image src={step1} width={400} height={300} className="w-[300px] sm:w-[400px] " alt="hero1"/>
             : item.id === 2
-            ? <span className="relative">
-               <Image src={step2} width={400} height={300} className="w-[300px] sm:w-[400px]"  alt="hero2"/>
-               <Image src={msg1}  width={400} height={300} className="top-[40%] left-[45%] absolute  w-[180px]"  alt="hero2"/>
-               <Image src={msg2} width={400} height={300} className="top-[60%] right-[45%] absolute w-[180px] "  alt="hero2"/>
+            ? <span className="relative ">
+               <Image src={step2} width={400} height={300} className="w-[300px] sm:w-[400px] me-2"  alt="hero2"/>
+               <Image src={msg1}  width={400} height={300} className="top-[40%] left-[47%] absolute  w-[160px]"  alt="hero2"/>
+               <Image src={msg2} width={400} height={300} className="top-[60%] right-[45%] absolute w-[160px] "  alt="hero2"/>
                    
             </span>
             : <Image src={step3} width={400} height={300} className="w-[300px] sm:w-[400px]"  alt="hero3"/>
 
           }
-           
-            <div className=" sm:w-[40%] mx-4 bg-white p-6 rounded-3xl shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px]">
+          <div className=" sm:w-[40%] mx-4 bg-white p-6 rounded-3xl shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px]">
               <h3 className="text-3xl font-bold mb-2">
                 Step<i className="text-clips">{item.id}:</i> {item.title}
               </h3>
@@ -34,6 +33,8 @@ function Hero() {
                 {item.description}
               </span>
             </div>
+           
+           
         
           </div>
         );
--- app/page.js
+++ app/page.js
@@ -1,9 +1,9 @@
 
-import Hero from "./components/Hero";
+import Hero from "./components/HeroN";
 import Navbar from "./components/Navbar";
 export default function Home() {
   return (
-    <main className="flex min-h-screen sm:w-[1200px] flex-col items-center m-auto justify-center">
+    <main className="flex min-h-screen sm:w-[1200px]  flex-col items-center m-auto justify-center">
           <Navbar/>
          <Hero/>
     </main>
